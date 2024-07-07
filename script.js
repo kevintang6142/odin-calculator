2360a4243c3ab9fb62914f7aa76c9ad47f0448a3
kevintang6142 <kevintang6142@gmail.com>
2024-07-07 12:35:16 -0500

    Implement four basic operations

--- script.js
+++ script.js
@@ -15,20 +15,78 @@ for (let i = 0; i <= 9; i++) {
 const decimal = document.querySelector("#decimal");
 const equals = document.querySelector("#equals");
 
-let operation = null;
-let previousNumber = "0";
-let currentNumber = "0";
+resetCalculator();
 
+clear.addEventListener("click", resetCalculator);
+operations.forEach(operation => operation.addEventListener("click", e => {
+    if (calculatorState === 2 && secondNumber !== null) calculate();
+    calculatorState = 2;
+    secondNumber = null;
+    currentOperation = operation.id;
+    operations.forEach(operation => operation.style = "");
+    operation.style.backgroundColor = "#ff8e2c";
+}));
+digits.forEach(digit => digit.addEventListener("click", e => updateNumber(e.target.id)));
+decimal.addEventListener("click", e => updateNumber("."));
+equals.addEventListener("click", e => {
+    calculate();
+    calculatorState = 3;
+});
 
-digits.forEach(digit => 
-    digit.addEventListener("click", e => updateDisplayNumber(e.target.id))
-);
-decimal.addEventListener("click", e => updateDisplayNumber("."));
+function updateNumber(userInput) {
+    if (calculatorState === 3) {
+        firstNumber = null;
+        calculatorState = 1;
+    }
+    if (calculatorState === 1){
+        if (firstNumber === null) firstNumber = "0";
+        if (firstNumber.length === 10) return;
+        if (firstNumber.includes(".") && userInput === ".") return;
+        firstNumber !== "0" || userInput === "." ? firstNumber = firstNumber + userInput : firstNumber = userInput;
+        display.innerText = firstNumber;
+    }
+    else if (calculatorState == 2) {
+        if (secondNumber === null) secondNumber = "0";
+        if (secondNumber.length === 10) return;
+        if (secondNumber.includes(".") && userInput === ".") return;
+        secondNumber !== "0" || userInput === "." ? secondNumber = secondNumber + userInput : secondNumber = userInput;
+        display.innerText = secondNumber;
+    }
+}
 
-function updateDisplayNumber(userInput) {
-    if (currentNumber.length === 10) return;
-    if (currentNumber.includes(".") && userInput === ".") return;   
-    if (currentNumber === "0" && userInput === "0") return;
-    currentNumber !== "0" || userInput === "." ? currentNumber = currentNumber + userInput : currentNumber = userInput;
-    display.innerText = currentNumber;
+function calculate() {
+    let result;
+    switch (currentOperation) {
+        case "add":
+            result = secondNumber !== null ? +firstNumber + +secondNumber : Math.abs(+firstNumber);
+            break;
+        case "subtract":
+            result = secondNumber !== null ? +firstNumber - +secondNumber : -Math.abs(+firstNumber);
+            break;
+        case "multiply":
+            if (secondNumber !== null) {
+                result = +firstNumber * +secondNumber
+            }
+            else {
+                result = firstNumber ** 2; 
+                secondNumber = firstNumber;
+            }
+            break;
+        case "divide":
+            result = secondNumber !== null ? +firstNumber / +secondNumber : 1 / +firstNumber;
+            break;
+        default:
+            result = +firstNumber;
+    }
+    firstNumber = result.toString();
+    display.innerText = firstNumber;
+}
+
+function resetCalculator() {
+    display.innerText = "0";
+    operations.forEach(operation => operation.style = "");
+    currentOperation = null;
+    firstNumber = null;
+    secondNumber = null;
+    calculatorState = 1;
 }
